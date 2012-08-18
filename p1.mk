46524072599f6c265885b0533f23a6422a29cdfd
Chirayu Desai <chirayudesai1@gmail.com>
2012-08-18 16:17:57 +0530

    move data to OneNAND
    Change-Id: Ifa0fef99723b09b2dcd9d3d270cac315cdec04c2

--- p1.mk
+++ p1.mk
@@ -82,10 +82,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
        ro.vold.switchablepair=/mnt/sdcard,/mnt/emmc \
        persist.sys.vold.switchexternal=0
 
-# keep dalvik cache on /data
-PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dexopt-data-only=1
-
 # set recovery.fstab location (needed for p1l & p1n products)
 TARGET_RECOVERY_FSTAB := device/samsung/p1/recovery.fstab
 
