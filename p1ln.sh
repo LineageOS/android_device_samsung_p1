b48e6878cef9a71a3d83f002f8d27bb523b99ca8
Chirayu Desai <cdesai@cyanogenmod.org>
2012-12-26 09:30:23 +0530

    p1ln: fixes
    Change-Id: Iab6985e962a871cb38e9057a1e2e10d1f2e02a47

--- p1ln.sh
+++ p1ln.sh
@@ -2,23 +2,15 @@
 
 HW=$(cat /proc/cpuinfo | grep Hardware | sed s/Hardware// | tr '[A-Z]' '[a-z]' | tr -d ' ' | tr -d ':' | tr -d '\t')
 DEVICE=$(cat /default.prop | grep ro.cm.device | sed s#ro\.cm\.device=## | tr '[A-Z]' '[a-z]')
+if [ "$DEVICE" == "" ] ; then
+mount /system
+DEVICE=$(system/bin/getprop ro.cm.device | tr '[A-Z]' '[a-z]')
+umount /system
+fi
 
 case $HW in
 p1|gt-p1000)
-	case $DEVICE in
-	p1)
-		model=p1
-	;;
-	p1l)
-		model=p1l
-	;;
-	p1n)
-		model=p1n
-	;;
-	*)
-	echo "Invalid device: $DEVICE"
-	;;
-	esac
+	model=p1
 ;;
 p1l|gt-p1000l)
 	model=p1l
@@ -31,6 +23,19 @@ echo "Invalid model: $HW"
 exit 2
 ;;
 esac
+
+if [ "$model" == "p1" ] ; then
+case $DEVICE in
+p1)
+;;
+p1l|p1n)
+	model=$DEVICE
+;;
+*)
+	echo "Invalid device: $DEVICE"
+;;
+esac
+fi
 
 if [ "$model" != "p1" ] ; then
 rm /tmp/boot.img
